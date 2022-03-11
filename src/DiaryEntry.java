5721f3af5d4a04154e116bac74157c5fcc8d36dd
DESKTOP-4D7T3LL\User <fatelan01@gmail.com>
2022-03-11 16:58:34 +0800

    DB connection and Add/Delete functions to database

--- src/DiaryEntry.java
+++ src/DiaryEntry.java
@@ -4,19 +4,17 @@ import java.util.Scanner;
 
 public class DiaryEntry {
     protected String title;
-    protected ArrayList<String> body;
+    protected String body;
     protected LocalDate date;
     protected User user;
+    protected int entryID;
 
-    public DiaryEntry(){
-
-    }
-    public DiaryEntry(User user){
-        body = new ArrayList<>();
+    public DiaryEntry(User user, int entryID){
         askTitle();
         askBody();
         setDate(LocalDate.now());
         setUser(user);
+        setEntryID(entryID);
         printEntry(); //for checking
     }
 
@@ -28,13 +26,31 @@ public class DiaryEntry {
         this.date = date;
     }
 
-    public void setBody(ArrayList<String> body){
+    public void setBody(String body){
         this.body = body;
     }
 
     public void setUser(User user){
         this.user = user;
     }
+
+    public void setEntryID(int entryID){
+        this.entryID = entryID;
+    }
+
+    public int getEntryID(){
+        return entryID;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getBody(){
+        return body;
+    }
+    public String getDate(){
+        return date.toString();
+    }
+
 
     public void askTitle(){
         System.out.println("Please enter entry your title. ");
@@ -44,27 +60,13 @@ public class DiaryEntry {
     }
 
     public void askBody(){
-
-        Boolean isNewLine = true;
-        while(isNewLine) {
-            System.out.println("Please enter entry your body. ");
-            Scanner in = new Scanner(System.in);
-            String body = in.nextLine();
-            addBody(body);
-
-            System.out.println("New line? 'y' to continue ");
-            Scanner in2 = new Scanner(System.in);
-            String newLineResponse = in2.nextLine();
-            if (!newLineResponse.equals("y")){
-                isNewLine = false;
-            }
-
-        }
+        System.out.println("Please enter entry your body. ");
+        Scanner in = new Scanner(System.in);
+        String body = in.nextLine();
+        setBody(body);
     }
 
-    public void addBody(String body){
-        this.body.add(body);
-    }
+
 
     public void printEntry(){
         printTitle();
@@ -78,9 +80,7 @@ public class DiaryEntry {
     }
 
     public void printBody(){
-        for (String body : this.body) {
-            System.out.println(body + "\n" );
-        }
+        System.out.println(body);
     }
 
     public void printDate(){
--- src/UI.java
+++ src/UI.java
@@ -26,5 +26,4 @@ public class UI {
         return in.nextLine();
     }
 
-
 }
